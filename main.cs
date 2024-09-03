d9b86aca4d86e5a727a56229dc7c10853e29aaef
ladkins14 <ladkins14@hawkmail.hccfl.edu>
2024-09-03 01:16:55 +0000

    Changed code to match Exercise 1.1 by having it write 4 lines displaying PersonalInfo

--- main.cs
+++ main.cs
@@ -2,6 +2,9 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Console.WriteLine ("Robert Hall");
+    Console.WriteLine ("2/27/1949");
+    Console.WriteLine ("Work 000-444-0101");
+    Console.WriteLine ("Cell 000-444-0189");
   }
 }
