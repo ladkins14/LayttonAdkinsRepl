61f1093ba6dc6d9287b54f4d3be7e81e99f6c692
ladkins14 <ladkins14@hawkmail.hccfl.edu>
2024-09-12 23:04:47 +0000

    I removed the WriteLines and added a named constant and two variables to calculate the number of centimeters in various inches. Finally I added a WriteLine to display the calculation in a sentence.

--- main.cs
+++ main.cs
@@ -1,11 +1,12 @@
 using System;
 
 class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Now, this is a story all about how");
-    Console.WriteLine ("My life got flipped-turned upside down");
-    Console.WriteLine ("And I'd like to take a minute");
-    Console.WriteLine ("Just sit right there");
-    Console.WriteLine ("I'll tell you how I became the prince of a town called Bel-Air");
+  private const double CENTIMETERS_PER_INCH = 2.54;
+  
+   public static void Main (string[] args) {
+   double measureInInches = 15994;
+   double total = measureInInches * CENTIMETERS_PER_INCH;
+   Console.WriteLine ("{0} inches is {1} centimeters.", measureInInches, total);
+  
   }
 }
