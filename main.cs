5b9ae603cf9bb751f9b2330c684734b232856e9f
ladkins14 <ladkins14@hawkmail.hccfl.edu>
2024-09-15 01:02:18 +0000

    Replaced constant with BASE_RATE, PER_HOUR, and PER_MILE, prompted user to input number of hours and miles and calculated the total moving fee.

--- main.cs
+++ main.cs
@@ -1,16 +1,23 @@
 using System;
 
 class Program {
-  private const double CENTIMETERS_PER_INCH = 2.54;
+  private const decimal BASE_RATE = 200;
+  private const decimal PER_HOUR = 150;
+  private const decimal PER_MILE = 2;
   
    public static void Main (string[] args) {
 
-   Console.WriteLine("Please Enter The Number Of Inches: ");
-   var inputNumber = Console.ReadLine();
-   double numberOfInches;
+   Console.WriteLine("Please Enter The Number Of Hours For Job: ");
+   var inputHours = Console.ReadLine();
+     
+   Console.WriteLine("Please Enter The Number Of Miles: ");
+   var inputMiles = Console.ReadLine();
+     
+   decimal numberOfHours;
+   decimal numberOfMiles;
    
-   if (double.TryParse(inputNumber, out numberOfInches)){
-     Console.WriteLine("{0} inches is {1} centimeters.", numberOfInches, (numberOfInches * CENTIMETERS_PER_INCH));
+   if (decimal.TryParse(inputHours, out numberOfHours) && decimal.TryParse(inputMiles, out numberOfMiles)) {
+     Console.WriteLine("Estimated Total Moving Fee is ${0}", (BASE_RATE + (PER_HOUR * numberOfHours) + (PER_MILE * numberOfMiles)));
      }
      else {
        Console.WriteLine("Please Input Numbers Only");
