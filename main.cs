061c1dc6e1d85264b088fc8e1f7a53b2cc2e8e43
ladkins14 <ladkins14@hawkmail.hccfl.edu>
2024-09-22 02:05:58 +0000

    I replaced the entire program with a user prompt and input, a line that reads the number of characters input, and an if-else statement that determines if the input was longer than 140 characters.

--- main.cs
+++ main.cs
@@ -1,27 +1,21 @@
 using System;
 
 class Program {
-  private const decimal BASE_RATE = 200;
-  private const decimal PER_HOUR = 150;
-  private const decimal PER_MILE = 2;
   
    public static void Main (string[] args) {
 
-   Console.WriteLine("Please Enter The Number Of Hours For Job: ");
-   var inputHours = Console.ReadLine();
-     
-   Console.WriteLine("Please Enter The Number Of Miles: ");
-   var inputMiles = Console.ReadLine();
-     
-   decimal numberOfHours;
-   decimal numberOfMiles;
+   Console.WriteLine("Please Enter Message: ");
+   var inputMessage = Console.ReadLine();
    
-   if (decimal.TryParse(inputHours, out numberOfHours) && decimal.TryParse(inputMiles, out numberOfMiles)) {
-     Console.WriteLine("Estimated Total Moving Fee is ${0}", (BASE_RATE + (PER_HOUR * numberOfHours) + (PER_MILE * numberOfMiles)));
-     }
-     else {
-       Console.WriteLine("Please Input Numbers Only");
-     }
-  
+     //Next line reads the number of characters in the input string 
+   int length = inputMessage.Length;
+
+   if (length <= 140) {
+    Console.WriteLine("Posted");
+   }
+
+   else if (length > 140) {
+    Console.WriteLine("Rejected");
+   }
   }
 }
