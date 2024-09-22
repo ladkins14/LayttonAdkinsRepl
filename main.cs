d318a7115156b736f62c40d427cb84618983e396
ladkins14 <ladkins14@hawkmail.hccfl.edu>
2024-09-22 20:21:59 +0000

    I changed the prompt, added a split to the ReadLine so users could also type units, had the program identify the number part of the input and change it from a string to an integer/decimal, and added multiple if-else if statements and an else statement to identify hurricane categories

--- main.cs
+++ main.cs
@@ -1,21 +1,44 @@
 using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Markup;
+using System.Xml.Schema;
 
 class Program {
   
    public static void Main (string[] args) {
 
-   Console.WriteLine("Please Enter Message: ");
-   var inputMessage = Console.ReadLine();
-   
-     //Next line reads the number of characters in the input string 
-   int length = inputMessage.Length;
+   Console.WriteLine("Please Enter Wind Speed: ");
+   // The .Split splits the users input from including both the value and the unit so the program can use the value
+   var inputWindSpeed = Console.ReadLine().Split();
+   // This line identifies the number part of the input
+   string input = string.Concat( inputWindSpeed.Where (Char.IsDigit));
+   // This line turns the input from a string into an integer/decimal
+   decimal outputInfo = decimal.Parse(input);
 
-   if (length <= 140) {
-    Console.WriteLine("Posted");
+   if (outputInfo >= 157) {
+    Console.WriteLine("Category Five Hurricane");
    }
 
-   else if (length > 140) {
-    Console.WriteLine("Rejected");
+   else if (outputInfo >= 130) {
+    Console.WriteLine("Category Four Hurricane");
    }
-  }
+
+   else if (outputInfo >= 111) {
+    Console.WriteLine("Category Three Hurricane");
+   }
+
+   else if (outputInfo >= 96) {
+    Console.WriteLine("Category Two Hurricane");
+   }
+
+   else if (outputInfo >= 74) {
+    Console.WriteLine("Category One Hurricane");
+   }
+   else {
+    Console.WriteLine("Not a Hurricane");
+   }
+
+ }
+
 }
