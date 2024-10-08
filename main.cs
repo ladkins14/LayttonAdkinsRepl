574e5ea9d97c6aacc709d9ceed449f294cf18856
ladkins14 <ladkins14@hawkmail.hccfl.edu>
2024-10-08 00:56:33 +0000

    I removed and re-added most of the code, with the main difference being three arrays containing salesperson names, allowed initials, and one that accepts the values earned by the salespersons The error message condition was changed to activate if input was not a value in the allowedInitials array and if statements check if input is a specific value in this array and increment it by that salespersons sales

--- main.cs
+++ main.cs
@@ -1,81 +1,97 @@
 using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
 class Program {
    public static void Main (string[] args) {
 
-    Char initial;
     string inputInitial;
     double dSaleAmount = 0;
     double eSaleAmount = 0;
     double fSaleAmount = 0;
-    double totalSaleAmount = 0;
+    double totalSaleAmount;
     Char highestSale = 'z';
 
+    string[] salesPersonNames = { "Danielle", "Edward", "Francis" };
+    string[] allowedInitials = {"D", "E", "F", "d", "e", "f", "Z", "z" };
+
+    //This array will hold the values of the sales each salesperson made, going from Danielle, to Edward, to Francis
+    double[] accumSales = new double[3];
+    
     while (true) {
+
       Console.Write("Please Enter Salesperson Initial or Z To Quit: ");
-
       inputInitial = Console.ReadLine();
-      //Next Line Converts user input into a character data type
-      initial = Convert.ToChar(inputInitial);
 
-      //This if statement displays an error message if the initial is not recognized
-      if (Char.ToUpper(initial) != 'D' && Char.ToUpper(initial) != 'E' && Char.ToUpper(initial) != 'F' && Char.ToUpper(initial) != 'Z') {
+      //This if statement displays an error message if the input is not one of the allowed initials or the exit case
+      if (!allowedInitials.Contains(inputInitial)) {
+
         Console.WriteLine("Error, invalid salesperson selected, please try again");
-
         inputInitial = Console.ReadLine();
-        initial = Convert.ToChar(inputInitial);
+
       }
 
-      //The next three if statements determine each salespersons sales based on the initial entered and user input
-        if (Char.ToUpper(initial) == 'D') {
+        //This if statement runs if the entered initial is equal to "D" or "d" in the array
+        if (inputInitial == allowedInitials[0] || inputInitial == allowedInitials[3]) {
 
-          Console.WriteLine("Enter Sale Amount: ");
+          Console.Write("Enter Sale Amount: ");
 
           dSaleAmount = double.Parse(Console.ReadLine());
 
-          totalSaleAmount = totalSaleAmount + dSaleAmount;
+          //This statement adds the amount of Danielle's sale to the first element of the accumSales array
+          accumSales[0] += dSaleAmount;
+
         }
 
-          if (Char.ToUpper(initial) == 'E') {
+          //This if statement runs if the entered initial is equal to "E" or "e" in the array
+          if (inputInitial == allowedInitials[1] || inputInitial == allowedInitials[4]) {
 
-            Console.WriteLine("Enter Sale Amount: ");
+            Console.Write("Enter Sale Amount: ");
 
-            eSaleAmount = double.Parse(Console.ReadLine());
+          eSaleAmount = double.Parse(Console.ReadLine());
 
-            totalSaleAmount = totalSaleAmount + eSaleAmount;
+          //This statement adds the amount of Edward's sale to the second element of the accumSales array
+          accumSales[1] += eSaleAmount;
+
           }
 
-           if (Char.ToUpper(initial) == 'F') {
+            //This if statement runs if the entered initial is equal to "F" or "f" in the array
+            if (inputInitial == allowedInitials[2] || inputInitial == allowedInitials[5]) {
 
-            Console.WriteLine("Enter Sale Amount: ");
+              Console.Write("Enter Sale Amount: ");
 
-            fSaleAmount = double.Parse(Console.ReadLine());
+              fSaleAmount = double.Parse(Console.ReadLine());
 
-            totalSaleAmount = totalSaleAmount + fSaleAmount;
-           }
+              //This statement adds the amount of Francis' sale to the third element of the accumSales array
+              accumSales[2] += fSaleAmount;
+            }
 
-            //These three if statements determine which salesperson made the most
-            if (dSaleAmount > eSaleAmount && dSaleAmount > fSaleAmount) {
+              //These three if statements determine which salesperson had the highest sale
+              if (dSaleAmount > eSaleAmount && dSaleAmount > fSaleAmount) {
               highestSale = 'D';
             }
 
-              if (eSaleAmount > dSaleAmount && eSaleAmount > fSaleAmount){
+                if (eSaleAmount > dSaleAmount && eSaleAmount > fSaleAmount){
                   highestSale = 'E';
               }
 
-                if (fSaleAmount > dSaleAmount && fSaleAmount > eSaleAmount){
+                  if (fSaleAmount > dSaleAmount && fSaleAmount > eSaleAmount){
                     highestSale = 'F';
                 }
 
-                  //This if statement is the exit condition and displays the total sales and the top seller
-                  if (Char.ToUpper(initial) == 'Z') {
+                    //This if statement is the exit condition and calculates the total sales and displays it and the top seller
+                    if (inputInitial == allowedInitials[6] || inputInitial == allowedInitials[7]) {
 
-                    Console.WriteLine("Grand Total: {0}", totalSaleAmount);
-                    Console.WriteLine("Highest Sale: {0}", highestSale);
-                    break;
-                  }
+                      totalSaleAmount = accumSales[0] + accumSales[1] + accumSales[2];
+                      Console.WriteLine("Grand Total: {0}", totalSaleAmount);
+                      Console.WriteLine("Highest Sale: {0}", highestSale);
+                      break;
+                    }
+        
+      }
              
     }
 
  }
 
-}
+
